4587ab9b9ea7f140d387bc1065f386c69ebad635
riku <cperez90@alumnes.politecnicllevant.cat>
2026-01-05 01:46:03 +0000

    Routes Reloaded - 6 Essential Tips

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
@@ -8,71 +9,35 @@ Route::get('/', function () {
 });
 
 //index
-Route::get('/jobs', function (){
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
+Route::get('/jobs', [JobController::class, 'index']);
 
 //create
 Route::get('/jobs/create', function (){
-    return view('jobs.create');
 });
 
 //show
-Route::get('/jobs/{id}', function ($id) {
+Route::get('/jobs/{job}', function (Job $job) {
 
-    $job = Job::find($id);
-
-    return view('jobs.show', ['job' => $job]);
 });
 
 //Store
 Route::post('/jobs', function() {
-    request()->validate([
-        'title'=>['required', 'min:3'],
-        'salary'=>['required']
-    ]);
 
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
 });
 
 //Edit
-Route::get('/jobs/{id}/edit', function ($id) {
+Route::get('/jobs/{job}/edit', function (Job $job) {
 
-    $job = Job::find($id);
-
-    return view('jobs.edit', ['job' => $job]);
 });
 
 // Update
-Route::patch('/jobs/{id}', function ($id) {
-    request()->validate([
-        'title'=>['required', 'min:3'],
-        'salary'=>['required']
-    ]);
+Route::patch('/jobs/{job}', function (Job $job) {
 
-    $job = Job::findOrFail($id);
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-
-    ]);
-    return redirect('/jobs/' . $job->id);
 });
 
 // Destroy
-Route::delete('/jobs/{id}', function ($id) {
-    Job::findOrFail($id)->delete();
-    return redirect('/jobs');
+Route::delete('/jobs/{job}', function (Job $job) {
+
 });
 
 Route::get('/contact', function () {
