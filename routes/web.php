a0ec0a348f9bb24245caee1b404ad28757918eb9
riku <cperez90@alumnes.politecnicllevant.cat>
2026-01-05 01:46:58 +0000

    Routes Reloaded - 6 Essential Tips

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,9 @@
 <?php
 
 use App\Http\Controllers\JobController;
-use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-//index
-Route::get('/jobs', [JobController::class, 'index']);
-
-//create
-Route::get('/jobs/create', function (){
-});
-
-//show
-Route::get('/jobs/{job}', function (Job $job) {
-
-});
-
-//Store
-Route::post('/jobs', function() {
-
-});
-
-//Edit
-Route::get('/jobs/{job}/edit', function (Job $job) {
-
-});
-
-// Update
-Route::patch('/jobs/{job}', function (Job $job) {
-
-});
-
-// Destroy
-Route::delete('/jobs/{job}', function (Job $job) {
-
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class);
