6ed6e57dffc0b502e0e4308bca4b587e7518b241
riku <cperez90@alumnes.politecnicllevant.cat>
2026-01-05 01:41:48 +0000

    Editing, Updating, and Deleting a Resource

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,16 +2,11 @@
     <x-slot:heading>
         Job Listings
     </x-slot:heading>
-
-    <div class="space-y-4">
-        @foreach($jobs as $job)
-            <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
-                <div class="font-bold text-blue-500 text-sm">{{$job->employer->name}}</div>
-                <div>
-                    <strong>{{$job['title']}}: </strong> Pays {{$job['salary']}} per year.
-                </div>
-            </a>
-        @endforeach
-    </div>
-
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+    <p>
+        This job pays {{ $job->salary }} per year.
+    </p>
+    <p class="mt-6">
+        <x-button href="/jobs/{{$job->id}}/edit">Edit Job</x-button>
+    </p>
 </x-layout>
