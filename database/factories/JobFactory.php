51c6422b5ecd8da9c88ebce991ee1c4231e58c9f
riku <cperez90@alumnes.politecnicllevant.cat>
2026-01-05 00:15:36 +0000

    Model Factories

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle(),
+            'employer_id' => Employer::factory(),
+            'salary' => '$50,000 USD'
+        ];
+    }
+}
